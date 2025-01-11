3e10d55c13c476306537bbb517ba138a1942ddde
iLox4 <ilyaumnovvv@gmail.com>
2025-01-11 17:45:22 +0100

    Make changes in fe. Introduce material ui -> change table and and stat data dysplay

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,12 +1,52 @@
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import LogsList from "./components/logsList/LogsList"
+import Grid from '@mui/material/Grid2'
+import { Box, Paper } from '@mui/material'
 
 function App() {
 
     return (
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <LogsList />
+        <Grid container spacing={4}>
+          <Grid size={8}>
+            <Paper elevation={2} sx={{ padding: 2, display: 'flex', flexDirection: 'column', gap: 3 }}>
+              <h1>Log list</h1>
+              <LogsList />
+            </Paper>
+          </Grid>
+          <Grid size={4}>
+            <Paper elevation={2} sx={{ padding: 2, display: 'flex', flexDirection: 'column', gap: 3 }}>
+              <h1>Log stats</h1>
+              <Grid container spacing={2}>
+                <Grid size={6}>
+                  <Box sx={{
+                    border: 1,
+                    borderColor: '#E0E0E0',
+                    borderRadius: 1,
+                    height: '7rem',
+                    padding: '10px'
+                  }}>
+                    <h3>Success percentage:</h3>
+                    <span style={{ fontSize: 'x-large' }}>75%</span>
+                  </Box>
+                </Grid>
+                <Grid size={6}>
+                  <Box sx={{
+                    border: 1,
+                    borderColor: '#E0E0E0',
+                    borderRadius: 1,
+                    height: '7rem',
+                    padding: '10px'
+                  }}>
+                    <h3>Number of users:</h3>
+                    <span style={{ fontSize: 'x-large' }}>10</span>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
       </LocalizationProvider>
     )
 }
--- fe/src/components/logsList/LogsList.tsx
+++ fe/src/components/logsList/LogsList.tsx
@@ -1,16 +1,54 @@
 // import InputForm from './inputForm/InputForm'
-import Table from '../ui/table/Table'
+// import Table from '../ui/table/Table'
 import LogSelectionForm from './LogSelectionForm'
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+// import Paper from '@mui/material/Paper';
 
 const LogsList = () => {
-    const colsHeaders = ["main", "secondary", "boo"]
-    const tableData = [{ mainVal: 22, secVal: 43, other: { boo: 24 }, id: 1}, { mainVal: 34, secVal: 55, other: { boo: 24 }, id: 2 }, {mainVal: 24, secVal: 45, other: { boo: 24 }, id: 3}]
-    const dataOrder = ['mainVal', 'secVal', { mainKey: 'other', secondaryKey: 'boo'}]
+    // const colsHeaders = ["main", "secondary", "boo"]
+    // const tableData = [{ mainVal: 22, secVal: 43, other: { boo: 24 }, id: 1}, { mainVal: 34, secVal: 55, other: { boo: 24 }, id: 2 }, {mainVal: 24, secVal: 45, other: { boo: 24 }, id: 3}]
+    // const dataOrder = ['mainVal', 'secVal', { mainKey: 'other', secondaryKey: 'boo'}]
+
+    const columns: GridColDef[] = [
+        {
+            field: 'id', headerName: 'ID', width: 70
+        },
+        {
+            field: 'user_id', headerName: 'user', width: 130
+        },
+        {
+            field: 'action', headerName: 'action', width: 130
+        },
+        {
+            field: 'timestamp', headerName: 'timestamp', width: 160
+        },
+        {
+            field: 'success', headerName: 'was successful', width: 90
+        },
+        {
+            field: 'fileName', headerName: 'file name', width: 130
+        },
+    ]
+
+    const rows = [
+        { id: '1', user_id: 'test@test1.com', action: 'generate CSV', timestamp: '2025-01-12', fileName: 'testFile.zip', success: true },
+        { id: '2', user_id: 'test2@test2.com', action: 'generate WB', timestamp: '2025-01-10', fileName: 'test2File.zip', success: true},
+        { id: '3', user_id: 'test@test3.com', action: 'generate CSV', timestamp: '2025-01-07', fileName: 'test3File.zip', success: false},
+        { id: '4', user_id: 'test@test1.com', action: 'generate WB', timestamp: '2025-01-08', fileName: 'test2File.zip', success: false},
+        { id: '5', user_id: 'test@test1.com', action: 'generate CSV', timestamp: '2025-01-10', fileName: 'testFile.zip', success: true},
+    ]
+
+    const paginationModel = { page: 0, pageSize: 5 };
 
     return (
         <>
             <LogSelectionForm/>
-            <Table colsHeaders={colsHeaders} tableData={tableData} dataOrder={dataOrder} />
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{ pagination: { paginationModel } }}
+                pageSizeOptions={[5, 10]}
+            />
         </>
     )
 }
